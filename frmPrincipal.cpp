bf8a912f6babb382d5450e150a29459e3662a49c
Igor Corona de Matos <coronaigor@gmail.com>
2020-10-03 18:34:20 -0300

    Primeiro commit, projeto adicionado.

---
+++ frmPrincipal.cpp
@@ -0,0 +1,16 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "frmPrincipal.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TFormPrincipal *FormPrincipal;
+//---------------------------------------------------------------------------
+__fastcall TFormPrincipal::TFormPrincipal(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
---
+++ frmPrincipal.h
@@ -0,0 +1,21 @@
+//---------------------------------------------------------------------------
+
+#ifndef frmPrincipalH
+#define frmPrincipalH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+//---------------------------------------------------------------------------
+class TFormPrincipal : public TForm
+{
+__published:	// IDE-managed Components
+private:	// User declarations
+public:		// User declarations
+	__fastcall TFormPrincipal(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TFormPrincipal *FormPrincipal;
+//---------------------------------------------------------------------------
+#endif
